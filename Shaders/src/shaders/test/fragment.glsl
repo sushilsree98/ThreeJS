40adecd6eb9bd39d2ebbbf6d410da6256ec50d7f
sushilsree98 <sushil.sree98@gmail.com>
2022-11-11 23:49:23 +0530

    Adding textures to shaders

--- Shaders/src/shaders/test/fragment.glsl
+++ Shaders/src/shaders/test/fragment.glsl
@@ -1,10 +1,15 @@
 precision mediump float;
 
 uniform vec3 uColor;
+uniform sampler2D uTexture;
 
 // varying float vRandom;
+varying vec2 vUv;
+varying float vElevation;
     
 void main()
 {
-    gl_FragColor = vec4(uColor, 1.0);
+    vec4 textureColor = texture2D(uTexture,vUv);
+    textureColor.rgb += vElevation * 1.5;
+    gl_FragColor = textureColor;
 }

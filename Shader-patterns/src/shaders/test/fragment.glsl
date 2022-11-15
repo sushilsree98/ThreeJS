af6909c2564f16750af75e85d747cb4c06c6800d
sushilsree98 <sushil.sree98@gmail.com>
2022-11-15 23:26:47 +0530

    Added shader patterns

--- Shader-patterns/src/shaders/test/fragment.glsl
+++ Shader-patterns/src/shaders/test/fragment.glsl
@@ -58,12 +58,29 @@ void main()
     // float strength = barX + barY;
 
     //Pattern 15
-    float barX = step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
-    barX *= step(0.4, mod(vUv.x * 10.0, 1.0));
+    // float barX = step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
+    // barX *= step(0.4, mod(vUv.x * 10.0, 1.0));
 
-    float barY = step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
-    barY *= step(0.4, mod(vUv.y * 10.0, 1.0));
-    float strength = barX + barY;
+    // float barY = step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
+    // barY *= step(0.4, mod(vUv.y * 10.0, 1.0));
+    // float strength = barX + barY;
+
+    // Pattern 16
+    //  float strength = abs(vUv.x - 0.5);
+
+    // Pattern 17
+    //  float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+
+     // Pattern 18
+    //  float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+
+    //Pattern 19
+    //  float strength = step(0.2,max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+
+    //Pattern 20
+     float strength1 = step(0.2,max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+     float strength2 = 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+     float strength = strength1 * strength2;
     
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
